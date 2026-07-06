84c1095388c57a1059d7e14f6f881f488ecdde83
Santhoshkumar0913 <santhoshkumarp2213@gmail.com>
2026-07-06 21:39:19 +0530

    Frontend foundation

---
+++ frontend/js/common.js
@@ -0,0 +1,27 @@
+const API_BASE = "http://localhost:7071/api";
+
+async function getData(endpoint){
+
+    const response = await fetch(`${API_BASE}/${endpoint}`);
+
+    return await response.json();
+
+}
+
+async function postData(endpoint,data){
+
+    const response = await fetch(`${API_BASE}/${endpoint}`,{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify(data)
+
+    });
+
+    return await response.json();
+
+}
